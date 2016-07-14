96029e06d4e98911da070c429bafb0e8a3f9b37e
Benjamin Hillmann <hillm096@teraminx.cs.umn.edu>
2016-07-14 15:41:28 -0500

    initialized the gitignore.

---
+++ scripts/download_refseq.sh
@@ -0,0 +1,5 @@
+python /project/flatiron/ben/NINJA-DOJO/ninja_dojo/scripts/download_refseq.py --output /project/flatiron/ben/ninja/refseq.archaea.fna --kingdoms archaea &
+python /project/flatiron/ben/NINJA-DOJO/ninja_dojo/scripts/download_refseq.py --output /project/flatiron/ben/ninja/refseq.bacteria.fna --kingdoms bacteria &
+python /project/flatiron/ben/NINJA-DOJO/ninja_dojo/scripts/download_refseq.py --output /project/flatiron/ben/ninja/refseq.fungi.fna --kingdoms fungi &
+python /project/flatiron/ben/NINJA-DOJO/ninja_dojo/scripts/download_refseq.py --output /project/flatiron/ben/ninja/refseq.viral.fna --kingdoms viral &
+python /project/flatiron/ben/NINJA-DOJO/ninja_dojo/scripts/download_refseq.py --output /project/flatiron/ben/ninja/refseq.protozoa.fna --kingdoms protozoa
