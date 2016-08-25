f87e76672dcf5d63013f808a617144bba3877542
Benjamin Hillmann <bhillmann@outlook.com>
2016-08-25 12:31:07 -0500

    Added the correct /

--- scripts/downsample_mp2_gold.sh
+++ scripts/downsample_mp2_gold.sh
@@ -2,13 +2,13 @@ k=(1000 10000 100000 1000000 10000000)
 
 for IN_FILE in *.fastq; do 
 
-    fastq_to_fastq -i ${IN_FILE} -o ${IN_FILE//.fastq/fna}
+    fastq_to_fastq -i ${IN_FILE} -o ${IN_FILE//fastq/fna}
 
     for i in ${k[@]}; do
         # Make the directory to store down-sampling results
         for j in `seq 1 5`; do
             # An experiment
-            subset_fasta -k ${i} -i ${IN_FILE//.fastq/fna} -o ${IN_FILE//.fastq}.i.j.fna
+            subset_fasta -k ${i} -i ${IN_FILE//fastq/fna} -o ${IN_FILE//.fastq}.i.j.fna
         done
     done
 done
