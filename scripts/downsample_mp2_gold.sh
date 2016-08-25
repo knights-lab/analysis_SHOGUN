56cd25f27c8eb0824c01d4f433cfe4747fcb6f22
Benjamin Hillmann <bhillmann@outlook.com>
2016-08-25 10:35:26 -0500

    Added the new analysis shogun.

--- scripts/downsample_mp2_gold.sh
+++ scripts/downsample_mp2_gold.sh
@@ -2,17 +2,13 @@ k=(1000 10000 100000 1000000 10000000)
 
 for IN_FILE in *.fastq; do 
 
-    # Grab the entire number of reads once for downsampling
-    NUM_READS="$(grep -c "^>" ${IN_FILE})"
-
-    mkdir ${IN_FILE//.fastq/}
+    fastq_to_fastq -i ${IN_FILE} -o ${IN_FILE//.fastq/fna}
 
     for i in ${k[@]}; do
         # Make the directory to store down-sampling results
-        test -d ${TRIAL_HOME}/hits_${i} | mkdir -p ${TRIAL_HOME}/hits_${i}
         for j in `seq 1 5`; do
             # An experiment
-            python /project/flatiron/ben/projects/NINJA-DOJO/ninja_dojo/scripts/subset_fasta.py -n ${NUM_READS} -k ${i} ${IN_FILE} > ${IN_FILE//.fastq/}/i.j.${IN_FILE}
+            subset_fasta -k ${i} -i {IN_FILE//.fastq/fna} -o ${IN_FILE//.fastq}.i.j.fna
         done
     done
 done
