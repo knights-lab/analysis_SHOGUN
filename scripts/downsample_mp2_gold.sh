11be4366f2acefcfe22127920be71917ae9c400b
Benjamin Hillmann <bhillmann@outlook.com>
2016-08-25 12:32:35 -0500

    Correct name for the file converter.

--- scripts/downsample_mp2_gold.sh
+++ scripts/downsample_mp2_gold.sh
@@ -2,7 +2,7 @@ k=(1000 10000 100000 1000000 10000000)
 
 for IN_FILE in *.fastq; do 
 
-    fastq_to_fastq -i ${IN_FILE} -o ${IN_FILE//fastq/fna}
+    fastq_to_fasta -i ${IN_FILE} -o ${IN_FILE//fastq/fna}
 
     for i in ${k[@]}; do
         # Make the directory to store down-sampling results
