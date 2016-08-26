1683819ef9fbd4770b7d6d7be1d43283a376fe64
Benjamin Hillmann <bhillmann@outlook.com>
2016-08-26 15:44:09 -0500

    Updated the convert fastq to fasta.

--- scripts/downsample_mp2_gold.sh
+++ scripts/downsample_mp2_gold.sh
@@ -1,14 +1,15 @@
 k=(1000 10000 100000 1000000 10000000)
 
-for IN_FILE in *.fastq; do 
-
+for IN_FILE in *.fastq; do
     fastq_to_fasta -i ${IN_FILE} -o ${IN_FILE//fastq/fna}
+done
 
+for IN_FILE in *.fna; do 
     for i in ${k[@]}; do
         # Make the directory to store down-sampling results
         for j in `seq 1 5`; do
             # An experiment
-            subset_fasta -k ${i} -i ${IN_FILE//fastq/fna} -o ${IN_FILE//.fastq}.i.j.fna
+            subset_fasta -k ${i} -i ${IN_FILE} -o ${IN_FILE//.fna}.i.j.fna
         done
     done
 done
