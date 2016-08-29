6d45ee85b992b4a6b104d26c51f919ab74eec8ce
Benjamin Hillmann <bhillmann@outlook.com>
2016-08-29 11:37:22 -0500

    Added to the downsample_mp2_gold create dir.

--- scripts/downsample_mp2_gold.sh
+++ scripts/downsample_mp2_gold.sh
@@ -4,12 +4,14 @@ for IN_FILE in *.fastq; do
     fastq_to_fasta -i ${IN_FILE} -o ${IN_FILE//fastq/fna}
 done
 
+mkdir subsample
+
 for IN_FILE in *.fna; do 
     for i in ${k[@]}; do
         # Make the directory to store down-sampling results
         for j in `seq 1 5`; do
             # An experiment
-            subset_fasta -k ${i} -i ${IN_FILE} -o ${IN_FILE//.fna}.i.j.fna
+            subset_fasta -k ${i} -i ${IN_FILE} -o subsample/${IN_FILE//.fna}.${i}.$[j}.fna
         done
     done
 done
