b2286120d1ea0849a6793de045c66ba882358d90
Benjamin Hillmann <bhillmann@outlook.com>
2017-03-30 14:30:28 -0500

    Added the BASE path

--- config.py
+++ config.py
@@ -2,14 +2,18 @@ import os
 
 ## Define variables
 
+BASE = "/project/flatiron2/ben/projects/analysis_SHOGUN/data"
+N_THREADS = 96
+
 ### scripts
-script_path = "scripts"
+script_path = os.path.join(BASE, "scripts")
 
 ### tools
-tool_path = "tools"
+tool_path = os.path.join(BASE, "tools")
 
 ### data
-data_path = "data"
+data_path = os.path.join(BASE, "data")
 
 ### results
-result_path = "results"
+result_path = os.path.join(BASE, "results")
+
